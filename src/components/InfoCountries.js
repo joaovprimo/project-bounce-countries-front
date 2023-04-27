449f86bc335dceb73f2f46cb120d36049662eb87
JOAO VITOR PRIMO DE OLIVEIRA <jv.primo26@gmail.com>
2023-04-27 18:33:31 +0100

    fix: reponsive design, ajusts at css informations country

--- src/components/InfoCountries.js
+++ src/components/InfoCountries.js
@@ -50,7 +50,6 @@ const Info = styled.div`
     color:black;
     display:flex;
     font-size:25px;
-    box-shadow: rgba(27, 31, 35, 0.04) 0px 1px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px inset;
     @media (max-width: 600px){
         font-size:15px;
     }
